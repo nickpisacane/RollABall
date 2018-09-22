3ce7b064e9342ffcd6040fb56a716142ac77cddc
Nicholas Pisacane <pisacanen@gmail.com>
2018-09-22 16:16:42 -0700

    add ui

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,17 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public Text countText;
+    public Text winText;
     private Rigidbody rb;
     private int count;
 
+    private void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
+        }
+    }
+
     void Start()
     {
-        count = 0;
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
     }
 
     void FixedUpdate()
@@ -28,8 +42,9 @@ public class PlayerController : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Pick Up"))
         {
-            count++;
             other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
         }
     }
 }
